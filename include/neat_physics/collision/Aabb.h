ebd468aa7570b43c58f012fe222e1eee15ae9ad3
Dmitry Sapelnikov <dmitry.sapelnikov@gmail.com>
2025-12-13 23:29:35 +0100

    Added: Aabb class

---
+++ include/neat_physics/collision/Aabb.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// Includes
+#include "neat_physics/math/Vec2.h"
+
+namespace nph
+{
+
+/// 2D axis-aligned bounding box, immutable for simplicity
+struct Aabb
+{
+	/// Minimum corner
+	const Vec2 min;
+
+	/// Maximum corner
+	const Vec2 max;
+
+	/// Default constructor (no initialization)
+	Aabb() noexcept = default;
+
+	/// Min-max point constructor. Asserts that min <= max
+	Aabb(
+		const Vec2& inMin,
+		const Vec2& inMax) noexcept :
+		min(inMin),
+		max(inMax)
+	{
+		assert(min.x <= max.x);
+		assert(min.y <= max.y);
+	}
+};
+
+// namespace nph
+}
