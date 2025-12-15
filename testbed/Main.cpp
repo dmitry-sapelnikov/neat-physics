38ab2076d0cd1f52165d369b1c7787845d581c5f
Dmitry Sapelnikov <ds@multiconsult.no>
2025-12-15 10:20:42 +0100

    Added: an initial scene setup

--- testbed/Main.cpp
+++ testbed/Main.cpp
@@ -1,35 +1,83 @@
 #include "Core.h"
 #include "Visualization.h"
+#include "neat_physics/World.h"
 
 using namespace nph;
+namespace
+{
 
-// Global functions
+/// Maximum number of bodies in the physics world
+static constexpr uint32_t MAX_BODIES = 1000;
+
+/// Gravity
+static constexpr float GRAVITY = 10.0f;
+
+/// Creates a 'glass-shaped' container
+void createGlass(
+	nph::World& world,
+	const nph::Vec2& glassSize,
+	float glassThickness,
+	float friction)
+{
+	// Create a static 'glass' made of 3 bodies: bottom and 2 sides
+	world.addBody(
+		{ glassSize.x + 2.0f * glassThickness, glassThickness },
+		0.0f,
+		friction,
+		{ 0.0f, -glassThickness * 0.5f });
+
+	world.addBody(
+		{ glassThickness, glassSize.y },
+		0.0f,
+		friction,
+		{ -(glassSize.x + glassThickness) * 0.5f, 0.5f * glassSize.y });
+
+	world.addBody(
+		{ glassThickness, glassSize.y },
+		0.0f,
+		friction,
+		{ (glassSize.x + glassThickness) * 0.5f, 0.5f * glassSize.y });
+}
+
+} // anonymous namespace
+
 /// The application entry point
 int main()
 {
 	try
 	{
-		Visualization* visualization = Visualization::getInstance();
+		nph::World world(MAX_BODIES);
+		const nph::Vec2 glassSize{ GRAVITY * 0.5f, GRAVITY };
+		createGlass(
+			world,
+			glassSize,
+			GRAVITY * 0.05f,
+			0.5f);
+
+		nph::Visualization* visualization = nph::Visualization::getInstance();
 		if (visualization == nullptr)
 		{
 			return -1;
 		}
+		visualization->setCameraZoom(int(glassSize.x * 2.0f));
+		visualization->setCameraPan(nph::Vec2(0.0f, glassSize.y * 0.5f));
 
+		nph::WorldDrawSettings drawSettings;
 		while (visualization->isRunning())
 		{
 			visualization->startFrame();
-			ImGui::ShowDemoWindow();
+			visualization->drawWorld(world, drawSettings);
 			visualization->endFrame();
 		}
 		return 0;
 	}
 	catch (const std::exception& exception)
 	{
-		logError("Exception caught: ", exception.what());
+		nph::logError("Exception caught: ", exception.what());
 	}
 	catch (...)
 	{
-		logError("Unknown exception caught.");
+		nph::logError("Unknown exception caught.");
 	}
 	return -1;
 }
