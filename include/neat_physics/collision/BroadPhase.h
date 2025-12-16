39804367a948dfa5ba93d4de2ceeb9f1b13993b3
Dmitry Sapelnikov <dmitry.sapelnikov@gmail.com>
2025-12-16 14:25:49 +0100

    Added: BroadPhase class

---
+++ include/neat_physics/collision/BroadPhase.h
@@ -0,0 +1,89 @@
+#pragma once
+
+// Includes
+#include <vector>
+#include "neat_physics/Body.h"
+#include "neat_physics/collision/Aabb.h"
+
+namespace nph
+{
+
+/// Broad-phase collision detection using sweep-and-prune algorithm
+class BroadPhase
+{
+public:
+	/// A pair of body indices
+	using BodyIndexPair = std::pair<uint32_t, uint32_t>;
+
+	/// Array of body index pairs
+	using BodyIndexPairArray = std::vector<BodyIndexPair>;
+
+	/// Array of Aabbs
+	using AabbArray = std::vector<Aabb>;
+
+	/// Constructor
+	BroadPhase(const BodyArray& bodies) noexcept :
+		mBodies(bodies)
+	{
+	}
+
+	/// Returns the pairs of bodies which AABBs are overlapping
+	[[nodiscard]] const BodyIndexPairArray& getCollidingPairs() const
+	{
+		return mCollidingPairs;
+	}
+
+	/// Returns the AABBs of the bodies
+	[[nodiscard]] const AabbArray& getAabbs() const noexcept
+	{
+		return mAabbs;
+	}
+
+	/// Updates the pairs of bodies which AABBs are overlapping
+	void update();
+
+private:
+	/// Endpoint of a segment
+	struct Endpoint
+	{
+		/// Coordinate value
+		float position;
+
+		/// Segment index
+		/// (1-based, positive for entry, negative for exit point)
+		int64_t index;
+
+		/// Operator < for sorting
+		bool operator<(const Endpoint& other) const noexcept
+		{
+			return position != other.position ?
+				position < other.position :
+				index < other.index;
+		}
+	};
+
+	/// Sweeps the AABBs along the X axis
+	void sweepAxis();
+
+	/// Reference to the bodies
+	const BodyArray& mBodies;
+
+	/// AABBs of the bodies
+	AabbArray mAabbs;
+
+	/// Endpoints for the sweep-and-prune algorithm
+	std::vector<Endpoint> mEndpoints;
+
+	/// Colliding pairs found in the last update
+	BodyIndexPairArray mCollidingPairs;
+
+	/// Active set of segment indices during the pruning phase
+	std::vector<uint32_t> mActivePoints;
+
+	/// Mapping for segment index - index in active set during
+	/// the pruning phase
+	std::vector<uint32_t> mActiveMapping;
+};
+
+// namespace nph
+}
---
+++ src/collision/BroadPhase.cpp
@@ -0,0 +1,122 @@
+// Includes
+#include "neat_physics/collision/BroadPhase.h"
+#include <algorithm>
+#include <iterator>
+
+namespace nph
+{
+
+namespace
+{
+
+/// Computes the AABB for a box-shaped body
+Aabb getAabb(const Body& body)
+{
+	const Mat22 absRotation = abs(body.rotation.getMat());
+	const Vec2 extents =
+		body.halfSize.x * absRotation.col1 +
+		body.halfSize.y * absRotation.col2;
+
+	return {
+		body.position - extents,
+		body.position + extents
+	};
+}
+
+} // anonymous namespace
+
+void BroadPhase::update()
+{
+	// Reserve-emplace because Aabb is immutable
+	mAabbs.clear();
+	mAabbs.reserve(mBodies.size());
+	for (size_t i = 0; i < mBodies.size(); ++i)
+	{
+		mAabbs.emplace_back(getAabb(mBodies[i]));
+	}
+
+	mActiveMapping.resize(mBodies.size());
+	// Very inefficient, but it is really actual since
+	// currently there is no body removal,only clearing all
+	if (mEndpoints.size() > mBodies.size() * 2)
+	{
+		mEndpoints.clear();
+	}
+
+	// Add endpoints for bodies that have been added since the last update
+	assert(mEndpoints.size() % 2 == 0);
+	for (int32_t i = static_cast<int32_t>(mEndpoints.size() >> 1);
+		 i < static_cast<int32_t>(mBodies.size()); ++i)
+	{
+		mEndpoints.emplace_back(0.0f, i + 1);
+		mEndpoints.emplace_back(0.0f, -(i + 1));
+	}
+
+	// Update endpoint positions
+	for (auto& endpoint : mEndpoints)
+	{
+		endpoint.position = endpoint.index > 0 ?
+			mAabbs[ endpoint.index - 1].min.x :
+			mAabbs[ -endpoint.index - 1].max.x;
+	}
+
+	// \todo Explicitly use the insertion sort
+	std::sort(mEndpoints.begin(), mEndpoints.end());
+	sweepAxis();
+}
+
+void BroadPhase::sweepAxis()
+{
+	mCollidingPairs.clear();
+	mActivePoints.clear();
+
+	const Body* bodyStart = mBodies.data();
+	const Aabb* aabbStart = mAabbs.data();
+	for (const auto& endpoint : mEndpoints)
+	{
+		if (endpoint.index > 0)
+		{
+			const uint32_t i1 = static_cast<uint32_t>(endpoint.index - 1);
+			const Body* bodyA = bodyStart + i1;
+			const Aabb* aabbA = aabbStart + i1;
+
+			for (uint32_t i2 : mActivePoints)
+			{
+				if (bodyA->isStatic() && (bodyStart + i2)->isStatic())
+				{
+					continue;
+				}
+
+				// If y-axes don't intersect
+				const Aabb* aabbB = aabbStart + i2;
+				if (aabbA->max.y < aabbB->min.y || 
+					aabbB->max.y < aabbA->min.y)
+				{
+					continue;
+				}
+
+				if (i1 < i2)
+				{
+					mCollidingPairs.emplace_back(i1, i2);
+				}
+				else
+				{
+					mCollidingPairs.emplace_back(i2, i1);
+				}
+			}
+			mActiveMapping[endpoint.index - 1] = static_cast<uint32_t>(mActivePoints.size());
+			mActivePoints.push_back(i1);
+		}
+		else
+		{
+			// Swap and pop
+			const uint32_t idx = mActiveMapping[-endpoint.index - 1];
+			const uint32_t last = mActivePoints.back();
+			mActivePoints[idx] = last;
+			mActiveMapping[last] = idx;
+			mActivePoints.pop_back();
+		}
+	}
+}
+
+} // namespace nph
