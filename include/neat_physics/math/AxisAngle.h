4643ba2da7e84b8863b8924f3d71b7f94f8a897a
Dmitry Sapelnikov <dmitry.sapelnikov@gmail.com>
2025-12-21 11:12:31 +0100

    Added: AxisAngle

---
+++ include/neat_physics/math/AxisAngle.h
@@ -0,0 +1,28 @@
+// A minimalistic 2D physics engine (https://github.com/dmitry-sapelnikov/neat-physics)
+// SPDX-FileCopyrightText: 2025 Dmitry Sapelnikov
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+// Includes
+#include <cstdint>
+
+namespace nph
+{
+
+/// Generic axis-angle trait
+template <uint16_t D>
+struct AxisAngleTrait;
+
+/// 2D axis-angle trait specialization
+template <>
+struct AxisAngleTrait<2>
+{
+	using Type = float;
+};
+
+/// Axis-angle definition via trait
+template <uint16_t D>
+using AxisAngle = typename AxisAngleTrait<D>::Type;
+
+} // namespace nph
